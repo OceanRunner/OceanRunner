e86dd7dab5aefd4264c5b8656dab57ab00dbb061
Max Langer <maxlanger@gmx.net>
2017-01-22 17:35:32 +0100

    Some scoring...

--- OceanRunner/Assets/Scripts/BoatLife.cs
+++ OceanRunner/Assets/Scripts/BoatLife.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class BoatLife : MonoBehaviour {
 
+	public GameObject lifeObj;
+	public GameObject scoreLabel;
+
 	public int life = 3;
 
+	private GameObject[] lifeObjs;
 	// Use this for initialization
 	void Start () {
-		
+		lifeObjs = new GameObject[life];
+		for (int i = 0; i < life; i++) {
+			lifeObjs [i] = Instantiate (lifeObj);
+			lifeObjs [i].transform.parent = Camera.main.transform;
+			lifeObjs [i].transform.position = new Vector3 (Utils.getCameraBounds().max.x - (i+1)*1.3f, Utils.getCameraBounds().max.y - 1f, -5f);
+		}
+		RectTransform rect = scoreLabel.GetComponent<RectTransform> ();
+		RectTransform parentRect = scoreLabel.transform.parent.gameObject.GetComponent<RectTransform> ();
+		rect.transform.position = new Vector3(-parentRect.rect.width / 2 + 450, parentRect.rect.height/2f-80, 0f);
 	}
 
 	void ResetBoat () {
@@ -22,9 +34,12 @@ public class BoatLife : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (transform.position.x < Utils.getCameraBounds().min.x || transform.position.x > Utils.getCameraBounds().max.x ||
-			(transform.rotation.eulerAngles.z < -100 && transform.rotation.eulerAngles.z > -180) || 
-			(transform.rotation.eulerAngles.z > 100 && transform.rotation.eulerAngles.z < 260) ) {
+			(transform.rotation.eulerAngles.z < -140 && transform.rotation.eulerAngles.z > -180) || 
+			(transform.rotation.eulerAngles.z > 140 && transform.rotation.eulerAngles.z < 220) ) {
 			life--;
+			if (life >= 0) {
+				lifeObjs [life].SetActive (false);
+			}
 			ResetBoat ();
 		} 
 	}
