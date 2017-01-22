7cc1dfdc296e6cd50c610f1ba6b51a9c4dc40800
ideaboxer <gyjoo3748@gmail.com>
2017-01-22 13:54:35 -0500

    Menu Scene Completed

---
+++ OceanRunner/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+	public void PushPlay() {
+		SceneManager.LoadScene(1);
+		//		Application.LoadLevel("Main");
+	}
+
+	public void Settings() {
+		SceneManager.LoadScene(2);
+		//		Application.LoadLevel("Main");
+	}
+	public void Exit() {
+		Application.Quit();
+	}
+}
+	
